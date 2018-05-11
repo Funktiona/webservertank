a9b92d0435de718778c49ec065a89e21218add54
macbookpro <Funktiona@gmail.com>
2018-05-11 10:30:43 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+import socket
+import time
+
+s = socket.socket()
+host = '192.168.1.239'
+# host = '192.168.0.123' #ip of raspberry pi
+port = 10000
+s.bind((host, port))
+
+c = None
+addr = None
+
+s.listen(5)
+while True:
+    c, addr = s.accept()
+    print('after')
+
+
+
+
+app = Flask(__name__, static_url_path='/static')
+socketio = SocketIO(app)
---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+            socket.on('connect', function() {
+            socket.emit('my event', {data: 'I\'m connected!'});
+            });
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+        e = e || window.event;
+
+        if (e.keyCode == '38') {
+                socket.emit('direction', 'up');
+        }
+        else if (e.keyCode == '40') {
+                socket.emit('direction', 'down');
+        }
+        else if (e.keyCode == '37') {
+                socket.emit('direction', 'left');}
+         else if (e.keyCode == '39') {
+                socket.emit('direction', 'right');
+        }
+
+}
+
+
